33be1d4ddda3a265fb49a26f9163cf7d1e99c6ca
Mahdi Ashouri <mahdi.eng77@gmail.com>
2020-04-04 07:40:48 -0700

    3rd commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 const firstSection = document.querySelector("#sec1");
 const secoundSection = document.querySelector("#sec2");
 const therdSection = document.querySelector("#sec3");
-
+const mainSection = document.querySelector(".container");
 let myHover1 = new hoverEffect({
   parent: therdSection,
   intensity: 0.2,
-  image1: "../dist/629506.jpg",
-  image2: "../dist/629508.jpg",
+  image1: "../dist/852939.jpg",
+  image2: "../dist/852939.jpg",
   displacementImage: "../dist/heightMap.png",
   imagesRatio: 960 / 540
 });
@@ -14,10 +14,10 @@ let myHover1 = new hoverEffect({
 let myHover2 = new hoverEffect({
   parent: secoundSection,
   intensity: 0.2,
-  image1: "../dist/738379.png",
-  image2: "../dist/629506s.png",
+  image1: "../dist/629508B.jpg",
+  image2: "../dist/629506B.jpg",
   displacementImage: "../dist/stripe1.png",
-  imagesRatio: 960 / 640
+  imagesRatio: 960 / 540
 });
 
 let myHover3 = new hoverEffect({
@@ -30,42 +30,51 @@ let myHover3 = new hoverEffect({
   angle: Math.PI / 8
 });
 const colors = [
-  "radial-gradient(#000, #000, #000, #0e0d0d)",
-  "radial-gradient(#220433, #13031c)",
-  "radial-gradient(#052b02, #021201)"
+  { bg: "#000", color: "#fff" },
+  { bg: "#34bed7", color: "#111" },
+  { bg: "#e1e4e9", color: "" }
 ];
 let currentPgae = 0;
+
 function init() {
   const tl = gsap.timeline();
   const aside = document.querySelectorAll(".items");
   const humbergur = document.querySelectorAll("#humbergur");
   const pages = document.querySelectorAll(".pages");
-  tl.fromTo(
-    pages[currentPgae],
-    0,
-    { opacity: 0, pointerEvents: "none" },
-    { opacity: 1, pointerEvents: "all" }
-  );
   aside.forEach((node, index) => {
     node.addEventListener("click", () => {
       activeClick(node);
       showMewPage(index);
     });
   });
-
-  function showMewPage(nextIndex) {
+  showMewPage(currentPgae, true);
+  function showMewPage(nextIndex, instart) {
     tl.fromTo(
       pages[currentPgae],
       0.5,
-      { opacity: 1, pointerEvents: "all" },
-      { opacity: 0, pointerEvents: "none" }
+      { opacity: 1, pointerEvents: "all", scale: 1 },
+      { opacity: 0, pointerEvents: "none", scale: 0.99 }
     )
+      .fromTo(
+        document.querySelector("body"),
+        0.2,
+        { color: colors[currentPgae].color },
+        { color: colors[nextIndex].color },
+        "-=2"
+      )
+      .fromTo(
+        mainSection,
+        !instart ? 0.6 : 0,
+        { background: colors[currentPgae].bg },
+        { background: colors[nextIndex].bg },
+        "-=1"
+      )
       .fromTo(
         pages[nextIndex],
         0.5,
         { opacity: 0, pointerEvents: "none", scale: 0.99 },
         { opacity: 1, pointerEvents: "all", scale: 1 },
-        "-=0.2"
+        "-=0.4"
       )
       .fromTo(
         pages[nextIndex].querySelector(".intro"),
@@ -80,17 +89,42 @@ function init() {
         { x: 0, y: 0, opacity: 1 },
         "-=0.2"
       )
-
       .set(document.querySelectorAll(".intro"), { clearProps: "all" });
-    // .set(pages[nextIndex], { clearProps: "all" });
     currentPgae = nextIndex;
   }
+
   function activeClick(item) {
     aside.forEach((node) => {
       node.classList.remove("active");
     });
     item.classList.add("active");
   }
+  document.addEventListener("wheel", throttle(scrollChange, 1500));
+  document.addEventListener("touchmove", throttle(scrollChange, 1500));
+  function scrollChange(e) {
+    const max_age = pages.length - 1;
+    let nextPage;
+    if (e.deltaY > 0) {
+      nextPage = currentPgae + 1 > max_age ? 0 : currentPgae + 1;
+    } else {
+      nextPage = currentPgae - 1 < 0 ? max_age : currentPgae - 1;
+    }
+    console.log(nextPage);
+    showMewPage(nextPage, false);
+  }
 }
 
 init();
+
+function throttle(func, limit) {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
